d095b480b47fb3d6d4fe042342b4d3000e01d53b
topocai <leonjoserp@gmail.com>
2024-05-07 22:58:26 -0300

    User profile component progress

--- frontend-side/src/components/UserProfile.jsx
+++ frontend-side/src/components/UserProfile.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react'
 
 import userServices from '../services/user'
+import './styles/user-profile.css'
 
 const UserProfile = ({ userId }) => {
   const [userInfo, setUserInfo] = useState(null)
@@ -19,17 +20,40 @@ const UserProfile = ({ userId }) => {
   const userAvatar = userInfo.UserData.user_avatar ? userInfo.UserData.user_avatar : 'https://placehold.co/500x500'
   const userName = userInfo.UserData.user_name
   const platforms = userInfo.UserData.user_platform.length > 0 ? userInfo.UserData.user_platform : null
-  const birthDate = userInfo.UserData.user_birthday ? userInfo.UserData.user_birthday : null
   const gender = userInfo.UserData.user_gender ? userInfo.UserData.user_gender : null
+
+  const gamesLists = userInfo.UserData.gamesList ? userInfo.UserData.gamesList : null
 
   return (
     <section>
-      <article>
-        <img src={userAvatar} alt={userName} />
-        <h1>{userName}</h1>
-        <p>{birthDate || 'Unknown'}</p>
-        <p>{gender || 'Unknown'}</p>
-        <p>{platforms || 'Unknown'}</p>
+      <article className='user-profile-container'>
+        <section className='user-profile-display'>
+          <figure className='user-profile-avatar'>
+            <img src={userAvatar} alt={userName} />
+          </figure>
+        </section>
+        <section className='user-profile-info'>
+          <div className='user-profile-name'>
+            <h2>{userName}</h2>
+            <p>{gender || 'Unknown'}</p>
+          </div>
+          <div>
+            <h3>Platforms</h3>
+            <div className='user-profile-platform'>
+              {platforms.map(platform => <span key={platform}>{platform}</span>)}
+            </div>
+          </div>
+          <div>
+            <h3>Games</h3>
+            <div>
+              {Object.keys(gamesLists).map(list => {
+                return (
+                  <span key={list}>{list}: {gamesLists[list].length}</span>
+                )
+              })}
+            </div>
+          </div>
+        </section>
       </article>
     </section>
   )
